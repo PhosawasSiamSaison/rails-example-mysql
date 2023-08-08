3d5300aa50020ad5155c21c41c8574478e154ad1
Phosawas <phosawas@siamsaison.com>
2023-08-08 15:11:33 +0700

    bundle

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -2,4 +2,4 @@ export PATH=/home/ubuntu/.rvm/gems/ruby-2.7.8/bin:/home/ubuntu/.rvm/gems/ruby-2.
 
 cd /var/www/rails-example-mysql/
 gem install bundler:2.1.4 — user-install
-# bundle install
+bundle install
