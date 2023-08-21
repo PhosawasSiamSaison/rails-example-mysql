08d95d854306eb1873195cc3d04ab386490a3aff
Phosawas <phosawas@siamsaison.com>
2023-08-21 13:10:16 +0700

    check env

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,7 +2,7 @@ class HelloController < ApplicationController
   def index
     render json: {
       message: "Hello welcome to rails-example-mysql",
-      env: ENV["RAILS_ENV"]
+      env:  Rails.env
     }
   end
 end
