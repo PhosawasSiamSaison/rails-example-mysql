1045be9aa31a37323d84d07169f1a17c3282d7a7
Phosawas <phosawas@siamsaison.com>
2023-08-21 10:32:54 +0700

    change version

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -1,4 +1,4 @@
-export PATH=/home/ubuntu/.rvm/gems/ruby-3.0.6/bin:/home/ubuntu/.rvm/gems/ruby-3.0.6@global/bin:/home/ubuntu/.rvm/rubies/ruby-3.0.6/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.rvm/bin:/home/ubuntu/.rvm/bin
+export PATH=/home/ubuntu/.rvm/gems/ruby-2.7.8/bin:/home/ubuntu/.rvm/gems/ruby-2.7.8@global/bin:/home/ubuntu/.rvm/rubies/ruby-2.7.8/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.rvm/bin:/home/ubuntu/.rvm/bin
 
 cd /var/www/rails-example-mysql/
 # gem install bundler — user-install
