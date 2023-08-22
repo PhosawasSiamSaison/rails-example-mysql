ae032fecd4ece7964488812ee217f3a6fe14609d
Phosawas <phosawas@siamsaison.com>
2023-08-22 13:13:40 +0700

    check env

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,7 +2,13 @@ class HelloController < ApplicationController
   def index
     render json: {
       message: "Hello welcome to rails-example-mysql",
-      env:  Rails.env
+      env:  Rails.env,
+      database: ENV['RDS_DATABASE'],
+      username: ENV['RDS_USERNAME'],
+      password: ENV['RDS_PASSWORD'],
+      host: ENV['RDS_HOST'],
+      port: 3306,
+      DATABASE_URL: ENV['DATABASE_URL'],
     }
   end
 end
