7329b30038a1af15a1cecb11ec2aaf8a9c70a660
Phosawas <phosawas@siamsaison.com>
2023-08-23 09:39:52 +0700

    change script

--- scripts/set_environment_variables.sh
+++ scripts/set_environment_variables.sh
@@ -2,7 +2,7 @@
 function set_parameter {
   SSM_PARAM_NAME=$1
   SSM_VALUE=`aws ssm get-parameters --name "${SSM_PARAM_NAME}" --query 'Parameters[*].Value' --output text`
-  [ "$(eval echo "$"$1)" == "" ] && echo "export ${SSM_PARAM_NAME}=${SSM_VALUE}" >> ~/.bashrc
+  [ "$(eval echo "$"$1)" == "" ] && echo "export ${SSM_PARAM_NAME}=${SSM_VALUE}" >> ~/.bash_profile
 }
 # echo "export RAILS_ENV=staging" >> ~/.bashrc
 set_parameter "RAILS_ENV"
